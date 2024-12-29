b5218bb0c87315d8cac9a0234cd46ce2f8dea655
Dawood Hameed <dawoodhameed8@gmail.com>
2024-12-29 15:04:22 +0500

    file uploads array

--- packages/shared/utils/fileUpload.ts
+++ packages/shared/utils/fileUpload.ts
@@ -6,38 +6,39 @@
 
 // const { aws_user_files_s3_bucket_region: region, aws_user_files_s3_bucket: bucket } = config;
 
-// export const fileUpload = async (files: any, path = '/common', compressedFile?: any) => {
-//   try {
-//     const urls = [];
-//     // eslint-disable-next-line no-plusplus
-//     for (let i = 0; i < files.length; i++) {
-//       let file = files[i];
-//       const name = file.name.split('.')[0];
-//       const { type: mimeType } = file;
-//       const type = mimeType.split('/').shift();
-//       const extension = mimeType.split('/').pop();
-//       const key = `media${path}/${type}-${uuid()}${+new Date()}name-${name}.${extension}`;
-//       let url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`;
-//       if (compressedFile) {
-//         file = await compressedFile(file);
-//       }
-//       // check for svg
-//       if (url.includes('.svg+xml')) {
-//         url = url.replace(/(svg)\+(xml)/g, '$1%2B$2');
-//       }
-//       await Storage.put(key, file, {
-//         contentType: mimeType,
-//       });
+export const fileUpload = async (files: any, path = '/common', compressedFile?: any) => {
+  try {
+    // const urls = [];
+    // // eslint-disable-next-line no-plusplus
+    // for (let i = 0; i < files.length; i++) {
+    //   let file = files[i];
+    //   const name = file.name.split('.')[0];
+    //   const { type: mimeType } = file;
+    //   const type = mimeType.split('/').shift();
+    //   const extension = mimeType.split('/').pop();
+    //   const key = `media${path}/${type}-${uuid()}${+new Date()}name-${name}.${extension}`;
+    //   let url = `https://${bucket}.s3.${region}.amazonaws.com/public/${key}`;
+    //   if (compressedFile) {
+    //     file = await compressedFile(file);
+    //   }
+    //   // check for svg
+    //   if (url.includes('.svg+xml')) {
+    //     url = url.replace(/(svg)\+(xml)/g, '$1%2B$2');
+    //   }
+    //   await Storage.put(key, file, {
+    //     contentType: mimeType,
+    //   });
 
-//       urls.push(url);
-//     }
-//     return urls;
-//   } catch (error) {
-//     // eslint-disable-next-line no-console
-//     console.log('file upload error', error.message);
-//     throw error;
-//   }
-// };
+    //   urls.push(url);
+    //}
+    //return urls;
+    return [];
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('file upload error', error.message);
+    throw error;
+  }
+};
 
 // export const getFileName = (url: string): string => {
 //   return url?.split('/')?.pop()?.split('name-')?.pop();
